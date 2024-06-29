3cf192b783264c56429daaf9d023903b8930f3dc
Liam Faruq <liam@Liams-MacBook-Pro.local>
2024-06-29 14:30:48 +0100

    add-priviledge-extension

--- programs/puppet-master/src/lib.rs
+++ programs/puppet-master/src/lib.rs
@@ -12,15 +12,6 @@ mod puppet_master {
     pub fn pull_strings(ctx: Context<PullStrings>, data: u64) -> Result<()> {
         puppet::cpi::set_data(ctx.accounts.set_data_ctx(), data)
     }
-
-    // pub fn pull_strings(ctx: Context<PullStrings>, data: u64) -> Result<()> {
-    //     let cpi_program = ctx.accounts.puppet_program.to_account_info();
-    //     let cpi_accounts = SetData {
-    //         puppet: ctx.accounts.puppet.to_account_info(),
-    //     };
-    //     let cpi_ctx = CpiContext::new(cpi_program, cpi_accounts);
-    //     puppet::cpi::set_data(cpi_ctx, data)
-    // }
 }
 
 #[derive(Accounts)]
@@ -28,13 +19,15 @@ pub struct PullStrings<'info> {
     #[account(mut)]
     pub puppet: Account<'info, Data>,
     pub puppet_program: Program<'info, Puppet>,
+    pub authority: Signer<'info>,
 }
 
 impl<'info> PullStrings<'info> {
     pub fn set_data_ctx(&self) -> CpiContext<'_, '_, '_, 'info, SetData<'info>> {
         let cpi_program = self.puppet_program.to_account_info();
         let cpi_accounts = SetData {
-            puppet: self.puppet.to_account_info()
+            puppet: self.puppet.to_account_info(),
+            authority: self.authority.to_account_info(),
         };
         CpiContext::new(cpi_program, cpi_accounts)
     }
